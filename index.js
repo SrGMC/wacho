c5b2f5d9cb4a58155196371e46354a8e15a337bc
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2022-05-09 00:23:46 +0200

    Improved server

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ function makeString(length) {
     return result;
 }
 
-const sequelize = new Sequelize("sqlite::memory", {
-    dialect: "sqlite",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     storage: "db.sqlite",
 });
 
@@ -34,20 +33,15 @@ const Party = sequelize.define("Party", {
 });
 
 const Item = sequelize.define("Item", {
-    uuid: {
-        type: DataTypes.UUIDV4,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-        unique: true,
-        primaryKey: true,
-    },
     tmdbId: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false,
+        primaryKey: true,
     },
     addedBy: {
         type: DataTypes.TEXT,
         allowNull: false,
+        primaryKey: true,
     },
     viewed: {
         type: DataTypes.BOOLEAN,
@@ -63,6 +57,12 @@ Party.hasMany(Item, { foreignKey: { name: "partyId", allowNull: false } });
 
 fastify.register(require("@fastify/static"), {
     root: path.join(__dirname, "dist"),
+    prefixAvoidTrailingSlash: true
+});
+
+fastify.register(require("fastify-language-parser"), {
+    supportedLngs: ["en"],
+    order: ['header']
 });
 
 async function sync() {
@@ -115,30 +115,45 @@ fastify.get("/api/v1/party/check", async (request, reply) => {
 });
 
 fastify.get("/api/v1/item/search", async (request, reply) => {
-    if (!request.query.q) {
+    if (!request.query.q || !request.query.partyId) {
         reply.status(400).send();
         return;
     }
+
+    var items = await Item.findAll({
+        where: {
+            partyId: request.query.partyId,
+        },
+    });
+
+    if (!items) {
+        reply.status(404).send({ status: 404, error: "Party not found" });
+        return;
+    }
+
+    let tmdbIds = items.map((item) => {
+        return parseInt(item.tmdbId);
+    });
 
     let result = await tmdb.searchMovie({ query: request.query.q });
 
-    reply.status(200).send(result);
-});
-
-fastify.get("/api/v1/item/get", async (request, reply) => {
-    if (!request.query.tmdbId) {
-        reply.status(400).send();
-        return;
-    }
-
-    tmdb.movieInfo({ id: request.query.tmdbId })
-        .then((res) => {
-            reply.status(200).send(res);
+    result = result.results
+        .map((item) => {
+            if (tmdbIds.includes(item.id)) {
+                return;
+            }
+            return {
+                tmdbId: item.id,
+                title: item.title,
+                overview: item.overview,
+                poster: "https://image.tmdb.org/t/p/w500/" + item.poster_path,
+                runtime: item.runtime,
+                url: item.homepage,
+            };
         })
-        .catch((res) => {
-            console.log(res);
-            reply.status(404).send({ status: 404, error: "Movie not found" });
-        });
+        .filter((e) => e);
+
+    reply.status(200).send(result);
 });
 
 fastify.put("/api/v1/item/add", async (request, reply) => {
@@ -146,6 +161,8 @@ fastify.put("/api/v1/item/add", async (request, reply) => {
         reply.status(400).send();
         return;
     }
+
+    request.body.tmdbId = parseInt(request.body.tmdbId);
 
     var party = await Party.findOne({
         where: {
@@ -196,6 +213,8 @@ fastify.post("/api/v1/item/viewed", async (request, reply) => {
         return;
     }
 
+    request.query.tmdbId = parseInt(request.query.tmdbId);
+
     var item = await Item.findOne({
         where: {
             partyId: request.query.partyId,
@@ -220,6 +239,8 @@ fastify.post("/api/v1/item/skipped", async (request, reply) => {
         return;
     }
 
+    request.query.tmdbId = parseInt(request.query.tmdbId);
+
     var item = await Item.findOne({
         where: {
             partyId: request.query.partyId,
@@ -233,6 +254,58 @@ fastify.post("/api/v1/item/skipped", async (request, reply) => {
     }
 
     item.skipped = true;
+    item.save();
+
+    reply.status(200).send();
+});
+
+fastify.delete("/api/v1/item/viewed", async (request, reply) => {
+    if (!request.query.partyId || !request.query.tmdbId) {
+        reply.status(400).send();
+        return;
+    }
+
+    request.query.tmdbId = parseInt(request.query.tmdbId);
+
+    var item = await Item.findOne({
+        where: {
+            partyId: request.query.partyId,
+            tmdbId: request.query.tmdbId,
+        },
+    });
+
+    if (!item) {
+        reply.status(404).send({ status: 404, error: "Item not found" });
+        return;
+    }
+
+    item.viewed = false;
+    item.save();
+
+    reply.status(200).send();
+});
+
+fastify.delete("/api/v1/item/skipped", async (request, reply) => {
+    if (!request.query.partyId || !request.query.tmdbId) {
+        reply.status(400).send();
+        return;
+    }
+
+    request.query.tmdbId = parseInt(request.query.tmdbId);
+
+    var item = await Item.findOne({
+        where: {
+            partyId: request.query.partyId,
+            tmdbId: request.query.tmdbId,
+        },
+    });
+
+    if (!item) {
+        reply.status(404).send({ status: 404, error: "Item not found" });
+        return;
+    }
+
+    item.skipped = false;
     item.save();
 
     reply.status(200).send();
@@ -253,6 +326,25 @@ fastify.get("/api/v1/item/list", async (request, reply) => {
     if (!items) {
         reply.status(404).send({ status: 404, error: "Party not found" });
         return;
+    }
+
+    items = items.map((item) => {
+        return {
+            partyId: item.partyId,
+            tmdbId: item.tmdbId,
+            viewed: item.viewed,
+            skipped: item.skipped,
+            addedBy: item.addedBy,
+        };
+    });
+
+    for (let i = 0; i < items.length; i++) {
+        let movie = await tmdb.movieInfo({ id: items[i].tmdbId, language: request.detectedLng });
+        items[i]["title"] = movie.title;
+        items[i]["overview"] = movie.overview;
+        items[i]["poster"] = "https://image.tmdb.org/t/p/w500/" + movie.poster_path;
+        items[i]["runtime"] = movie.runtime;
+        items[i]["url"] = movie.homepage;
     }
 
     reply.status(200).send(items);
@@ -279,6 +371,27 @@ fastify.get("/api/v1/item/random", async (request, reply) => {
 
     let item = items[Math.floor(Math.random() * items.length)];
 
+    let movie = await tmdb.movieInfo({ id: item.tmdbId, language: request.detectedLng });
+    let watchProviders = await tmdb.movieWatchProviders({ id: item.tmdbId });
+    countryProviders = watchProviders.results[request.headers["CF-IPCountry"]];
+    if (!countryProviders) {
+        countryProviders = watchProviders.results["GB"];
+    }
+
+    item = {
+        partyId: item.partyId,
+        tmdbId: item.tmdbId,
+        viewed: item.viewed,
+        skipped: item.skipped,
+        addedBy: item.addedBy,
+        title: movie.title,
+        overview: movie.overview,
+        poster: "https://image.tmdb.org/t/p/original/" + movie.poster_path,
+        runtime: movie.runtime,
+        url: movie.homepage,
+        providers: countryProviders.link,
+    };
+
     reply.status(200).send(item);
 });
 
@@ -291,7 +404,7 @@ const start = async () => {
         process.exit(1);
     }
     try {
-        await fastify.listen(3000, '0.0.0.0');
+        await fastify.listen(process.env.PORT || 3000, "0.0.0.0");
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
