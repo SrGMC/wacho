0afbc7b138720cce2f8fdd415e85c42a50232fd2
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2022-06-19 18:37:59 +0200

    Implement db fallback

--- libs/sequelize.db.js
+++ libs/sequelize.db.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || "sqlite::memory", {
     storage: 'db.sqlite',
 });
 
